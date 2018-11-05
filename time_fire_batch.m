d38a1b7bd7bbb5b839e9aebe5c4bd9cd32f6aca3
eringiglio <eringiglio@gmail.com>
2018-11-05 10:13:42 -0600

    Modifying ability to fire a zBUS trigger as a self-contained event.

---
+++ time_fire_batch.m
@@ -0,0 +1,11 @@
+function [] = time_fire_batch(xlsFile,)
+
+This function takes an Excel file and passes the times listed inside to timed_fire in order to create a list of playout fires (zBUS A triggers) for two chambers as specified in a paired RPvdsEx program.
+
+zBUS = actxcontrol('ZBUS.x',[1,1,1,1]);
+if zBus.ConnectZBUS('GB')
+  e = 'connected'
+else
+  e='unable to connect'
+end
+zBUS.FluishIO(1);
--- timed_fire.m
+++ timed_fire.m
@@ -1,6 +1,17 @@
-function [] = timed_fire(year,month,day,hour,minute,second)
-% This function is designed to take an input of the dateTime and then fire 
-%a ZBus A trigger that should generate the playout of a song. 
+function [] = timed_fire(year,month,day,hour,minute,second,zBUS)
+% This function is designed to take an input of the dateTime and then fire
+%a ZBus A trigger that should generate the playout of a song.
+% Erin M Giglio, 2018
 
-t = timer('TimerFcn',@(~,~)disp('Test'));
+if nargin < 7
+  zBUS = actxcontrol('ZBUS.x',[1,1,1,1]);
+  if zBus.ConnectZBUS('GB')
+    e = 'connected'
+  else
+    e='unable to connect'
+  end
+  zBUS.FluishIO(1);
+end
+
+t = timer('TimerFcn',@(~,~)zBUS.zBusTrigA(0,0,5);
 startat(t,2018,11,2,18,26,00);
