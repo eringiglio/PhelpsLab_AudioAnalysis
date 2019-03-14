a1a18ffdde64ece647847875b900f5d3c86b0784
eringiglio <eringiglio@gmail.com>
2019-03-14 10:49:25 -0500

    Updating program to take either already-running zBUS or one passed to it, with trigger control options

--- timed_fire.m
+++ timed_fire.m
@@ -1,10 +1,10 @@
-function [] = timed_fire(year,month,day,hour,minute,second,zBUS)
+function [] = timed_fire(year,month,day,hour,minute,second,trigger,zBUS)
 % This function is designed to take an input of the dateTime and then fire
 %a ZBus A trigger that should generate the playout of a song.
 
 % Erin M Giglio, March 2019
 
-if nargin < 7
+if nargin < 8
   zBUS = actxcontrol('ZBUS.x',[1,1,1,1]);
   if zBus.ConnectZBUS('GB')
     e = 'connected'
@@ -13,18 +13,10 @@ if nargin < 7
   end
   zBUS.FluishIO(1);
 
-end
-raw = csvimport(csvfile);
-times = raw(2:21,:);
-
-[r,c] = size(times);
-
-for i=1:r
-    t(i) = timer('TimerFcn',@(~,~)zBUS.zBusTrigA(0,0,5));
-    y(i) = times{i,1};
-    mo(i) = times{i,2};
-    d(i) = times{i,3};
-    h(i) = times{i,4};
-    mi(i) = times{i,5};
-    s(i) = times{i,6};
-startat(t,y,mo,d,h,mi,s);
+if trigger == 'A'
+  t = timer('TimerFcn',@(~,~)zBUS.zBusTrigA(0,0,5));
+elseif trigger == 'B'
+  t = timer('TimerFcn',@(~,~)zBUS.zBusTrigB(0,0,5));
+else
+  error('Please enter A or B for trigger.')
+startat(t,year,month,day,hour,minute,second);
